a92ac811ed48752eb59819ae2a3cdc4da47cd670
Andrew <andrew@Andrews-MacBook-Pro.local>
2022-03-06 01:30:53 +0800

    fix addresses for coord-server and server-server RPCs

--- chainedkv/server.go
+++ chainedkv/server.go
@@ -125,13 +125,13 @@ type ServerJoiningArgs struct {
 }
 
 type ServerJoiningRes struct {
-	tailServerListenAddr string
+	tailServerListenAddr string // this server -> (tail) server
 	sToken               tracing.TracingToken
 }
 
 type NextServerJoiningArgs struct {
 	nextServerId         uint8
-	nextServerListenAddr string
+	nextServerListenAddr string // (callee) server -> this server
 	sToken               tracing.TracingToken
 }
 
@@ -142,7 +142,8 @@ type NextServerJoiningRes struct {
 type ServerJoinedArgs struct {
 	serverId         uint8
 	ackAddr          string
-	serverListenAddr string
+	coordListenAddr  string // coord -> this server
+	serverListenAddr string // (caller) server -> this server
 	sToken           tracing.TracingToken
 }
 
@@ -151,14 +152,12 @@ type ServerJoinedRes struct {
 }
 
 type Server struct {
-	mutex  sync.Mutex // restrict access to server state
-	strace *tracing.Tracer
-	isHead bool
-	isTail bool
-	//predecessorId         uint8  // only valid when !isHead?
+	mutex                 sync.Mutex // restrict access to server state
+	strace                *tracing.Tracer
+	isHead                bool
+	isTail                bool
 	predecessorListenAddr string // only valid when !isHead?
-	//successorId           uint8  // only valid when !isTail?
-	successorListenAddr string // only valid when !isTail?
+	successorListenAddr   string // only valid when !isTail?
 }
 
 func NewServer() *Server {
@@ -169,30 +168,18 @@ func NewServer() *Server {
 }
 
 func (s *Server) Start(serverId uint8, coordAddr string, serverAddr string, serverServerAddr string, serverListenAddr string, clientListenAddr string, strace *tracing.Tracer) error {
+	// Initialize trace
 	s.strace = strace
 	trace := strace.CreateTrace()
 	trace.RecordAction(ServerStart{serverId})
 
-	/* Connect to coord */
-	// Connection establishment code adapted from Piazza post @471_f1
-	serverTCPAddr, err := net.ResolveTCPAddr("tcp", serverAddr)
-	if err != nil {
-		return err
-	}
-	coordTCPAddr, err := net.ResolveTCPAddr("tcp", coordAddr)
-	if err != nil {
-		return err
-	}
-	cConn, err := net.DialTCP("tcp", serverTCPAddr, coordTCPAddr)
-	if err != nil {
-		return err
-	}
+	// Connect to coord
+	cConn, cClient, err := establishRPCConnection(serverAddr, coordAddr)
 	defer cConn.Close()
-	cClient := rpc.NewClient(cConn)
 	defer cClient.Close()
 
 	/* Join */
-	// Send join request to coord; receive current tail and updated trace token
+	// Send join request to coord; receive current tail
 	trace.RecordAction(ServerJoining{serverId})
 	serverJoiningArgs := &ServerJoiningArgs{
 		serverId: serverId,
@@ -203,49 +190,57 @@ func (s *Server) Start(serverId uint8, coordAddr string, serverAddr string, serv
 	if err != nil {
 		return err
 	}
-	tailServerListenAddr := serverJoiningRes.tailServerListenAddr
 	sToken := serverJoiningRes.sToken
+	s.predecessorListenAddr = serverJoiningRes.tailServerListenAddr
+	s.isHead = s.predecessorListenAddr != ""
 
-	// If tail exists, tell tail that I am new tail; receive updated trace token
-	if tailServerListenAddr != "" {
-		tailClient, err := rpc.Dial("tcp", tailServerListenAddr)
+	// If I am not head, tell predecessor (previous tail) that I am new tail
+	if s.isHead {
+		predConn, predClient, err := establishRPCConnection(serverServerAddr, s.predecessorListenAddr)
 		if err != nil {
 			return err
 		}
-		defer tailClient.Close()
+		defer predConn.Close()
+		defer predClient.Close()
+
 		nextServerJoiningArgs := &NextServerJoiningArgs{
 			nextServerId:         serverId,
 			nextServerListenAddr: serverListenAddr,
 			sToken:               sToken,
 		}
 		var nextServerJoiningRes NextServerJoiningRes
-		err = tailClient.Call("Server.AddSuccessor", nextServerJoiningArgs, &nextServerJoiningRes)
+		err = predClient.Call("Server.AddSuccessor", nextServerJoiningArgs, &nextServerJoiningRes)
 		if err != nil {
 			return err
 		}
 		sToken = nextServerJoiningRes.sToken
 	}
 
-	// Update server state
-	s.isHead = tailServerListenAddr == ""
-	s.predecessorListenAddr = tailServerListenAddr
-
 	// Start listening to heartbeats on random port
-	ackIP := strings.Split(serverAddr, ":")[0]
-	ackAddr := fchecker.StartListener(fmt.Sprint(ackIP, ":"))
+	serverIP := strings.Split(serverAddr, ":")[0]
+	serverAckAddr := fchecker.StartListener(fmt.Sprint(serverIP, ":"))
 
 	// Start listening for RPCs
-	err = s.handleRPC(serverListenAddr)
+	err = rpc.Register(s)
+	if err != nil {
+		return err
+	}
+	_, err = startRPCListener(serverListenAddr) // server -> server RPC
+	if err != nil {
+		return err
+	}
+	coordListenAddr, err := startRPCListener(fmt.Sprint(serverIP, ":")) // coord -> server RPC
 	if err != nil {
 		return err
 	}
 
-	// Notify coord that I have successfully joined; receive updated trace token
+	// Notify coord that I have successfully joined
 	trace = strace.ReceiveToken(sToken)
 	trace.RecordAction(ServerJoined{serverId})
 	serverJoinedArgs := &ServerJoinedArgs{
 		serverId:         serverId,
-		ackAddr:          ackAddr.String(),
+		ackAddr:          serverAckAddr.String(),
+		coordListenAddr:  coordListenAddr.String(),
 		serverListenAddr: serverListenAddr,
 		sToken:           trace.GenerateToken(),
 	}
@@ -259,6 +254,37 @@ func (s *Server) Start(serverId uint8, coordAddr string, serverAddr string, serv
 	return errors.New("not implemented")
 }
 
+func establishRPCConnection(laddr, raddr string) (*net.TCPConn, *rpc.Client, error) {
+	// Code adapted from Piazza post @471_f1
+	resolvedLaddr, err := net.ResolveTCPAddr("tcp", laddr)
+	if err != nil {
+		return nil, nil, err
+	}
+	resolvedRaddr, err := net.ResolveTCPAddr("tcp", raddr)
+	if err != nil {
+		return nil, nil, err
+	}
+	conn, err := net.DialTCP("tcp", resolvedLaddr, resolvedRaddr)
+	if err != nil {
+		return nil, nil, err
+	}
+	client := rpc.NewClient(conn)
+	return conn, client, nil
+}
+
+func startRPCListener(rpcListenAddr string) (net.Addr, error) {
+	resolvedRPCListenAddr, err := net.ResolveTCPAddr("tcp", rpcListenAddr)
+	if err != nil {
+		return nil, err
+	}
+	listener, err := net.ListenTCP("tcp", resolvedRPCListenAddr)
+	if err != nil {
+		return nil, err
+	}
+	go rpc.Accept(listener)
+	return listener.Addr(), nil
+}
+
 func (s *Server) AddSuccessor(args *NextServerJoiningArgs, reply *NextServerJoiningRes) error {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
@@ -270,23 +296,6 @@ func (s *Server) AddSuccessor(args *NextServerJoiningArgs, reply *NextServerJoin
 
 	trace.RecordAction(NewJoinedSuccessor{args.nextServerId})
 	*reply = NextServerJoiningRes{trace.GenerateToken()}
-	return nil
-}
-
-func (s *Server) handleRPC(serverListenAddr string) error {
-	err := rpc.Register(s)
-	if err != nil {
-		return err
-	}
-	resolvedServerListenAddr, err := net.ResolveTCPAddr("tcp", serverListenAddr)
-	if err != nil {
-		return err
-	}
-	listener, err := net.ListenTCP("tcp", resolvedServerListenAddr)
-	if err != nil {
-		return err
-	}
-	go rpc.Accept(listener)
 	return nil
 }
 
