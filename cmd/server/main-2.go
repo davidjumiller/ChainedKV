acea97503c85a63ff3c1d5cf32852890e0d848c5
amy liu <amyliu45@gmail.com>
2022-03-11 20:31:19 -0800

    Added more servers for multi-server chain testing

---
+++ cmd/server/main-2.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"cs.ubc.ca/cpsc416/a3/chainedkv"
+	"cs.ubc.ca/cpsc416/a3/util"
+	"github.com/DistributedClocks/tracing"
+)
+
+func main() {
+	var config chainedkv.ServerConfig
+	util.ReadJSONConfig("config/server_config_2.json", &config)
+	stracer := tracing.NewTracer(tracing.TracerConfig{
+		ServerAddress:  config.TracingServerAddr,
+		TracerIdentity: config.TracingIdentity,
+		Secret:         config.Secret,
+	})
+	server := chainedkv.NewServer()
+	server.Start(config.ServerId, config.CoordAddr, config.ServerAddr, config.ServerServerAddr, config.ServerListenAddr, config.ClientListenAddr, stracer)
+}
